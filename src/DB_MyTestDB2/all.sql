1e94c12cf623013f899c7618090e29fe2c0198fd
Pedro Akira <40009054+pedroAkiraDanno@users.noreply.github.com>
2023-11-01 09:54:54 -0300

    Create all.sql

---
+++ src/DB_MyTestDB2/all.sql
@@ -0,0 +1,25 @@
+﻿USE [master]
+GO
+
+/****** Object:  Database [MyTestDB2]    Script Date: 10/31/2023 8:39:49 AM ******/
+CREATE DATABASE [MyTestDB2];
+GO
+
+
+  
+
+
+
+   
+USE  [MyTestDB2]
+GO
+
+﻿CREATE TABLE [dbo].[UserTab]
+(
+	  [Id] INT NULL PRIMARY KEY, 
+    [Name] NCHAR(100) NULL, 
+    [Age] FLOAT NULL
+)
+GO
+
+
